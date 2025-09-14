db8b9798c4f46324c73fb4642104e29bbe84d6cc
Aadhavan Srinivasan <aadhavan@twomorecents.org>
2025-09-14 16:45:02 -0400

    Add URL class and main file to parse URLs

---
+++ main.py
@@ -0,0 +1,30 @@
+from url import Url
+import sys
+
+def parseUrlFile(urlFile: str) -> list[Url]:
+    f = open(urlFile, "r")
+    url_list : list[Url] = list()
+
+    link_list : list[str] = f.read().split("\n")
+    for link in link_list:
+        if link == '': # Empty link ie. empty line
+            continue
+        url_list.append(Url(link))
+    f.close()
+    return url_list
+
+
+def main():
+    if (len(sys.argv)) != 2:
+        print("URL_FILE is a required argument.")
+        return 1
+
+    urlFile = sys.argv[1]
+    urls : list[Url] = parseUrlFile(urlFile)
+    for url in urls:
+        print(url)
+
+    return 0
+
+if __name__ == '__main__':
+    main()
